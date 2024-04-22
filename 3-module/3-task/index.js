68bb5b82b7a0af621394eaf18c26971461aa6160
Vampika <vampika.vamp@gmail.com>
2024-04-22 04:08:33 +0300

    3-3, 3-4, 3-5

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,7 @@
 function camelize(str) {
   // ваш код...
+  let temp = str.split('-');
+  return temp
+    .map((t, index) => {if(index > 0 && t.length > 0) {console.log(index); return String.fromCharCode(t[0].charCodeAt() - 32) + t.slice(1)} else return t})
+    .join('');
 }
--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,3 +1,6 @@
 function showSalary(users, age) {
   // ваш код...
+  let temp = '';
+  users.forEach(user => {if(user.age <= age) temp+=user.name + ', ' + user.balance + '\n'})
+  return temp.trim();
 }
