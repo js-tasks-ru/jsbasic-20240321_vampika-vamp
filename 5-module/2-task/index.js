77dc159555d444f0cbcfac2e9223c7dcf75a52c0
Vampika <vampika.vamp@gmail.com>
2024-05-01 01:35:37 +0300

    5-2

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,11 @@
 function toggleText() {
   // ваш код...
+  let toggleButton = document.querySelector(".toggle-text-button");
+  toggleButton.onclick = function () {
+    let elem = document.querySelector("#text");
+    if(elem.hidden)
+      elem.hidden = false;
+    else
+      elem.hidden = true;
+  }
 }
